5aff41a3125d91189cecf7d520234ef3a4726dee
Shanjai Rathinam <rathinamshanjai@gmail.com>
2025-11-05 11:13:47 +0530

    Add ELF64 structures and write function prototype

---
+++ include/elf.h
@@ -0,0 +1,52 @@
+#ifndef ELF_H
+#define ELF_H
+
+#include <vector>
+#include <string>
+#include <cstdint>
+
+// Simplified ELF64 structures
+using Elf64_Addr = uint64_t;
+using Elf64_Off = uint64_t;
+using Elf64_Half = uint16_t;
+using Elf64_Word = uint32_t;
+using Elf64_Sword = int32_t;
+using Elf64_Xword = uint64_t;
+using Elf64_Sxword = int64_t;
+
+// ELF Header
+struct Elf64_Ehdr {
+    unsigned char e_ident[16];
+    Elf64_Half    e_type;
+    Elf64_Half    e_machine;
+    Elf64_Word    e_version;
+    Elf64_Addr    e_entry;
+    Elf64_Off     e_phoff;
+    Elf64_Off     e_shoff;
+    Elf64_Word    e_flags;
+    Elf64_Half    e_ehsize;
+    Elf64_Half    e_phentsize;
+    Elf64_Half    e_phnum;
+    Elf64_Half    e_shentsize;
+    Elf64_Half    e_shnum;
+    Elf64_Half    e_shstrndx;
+};
+
+// Section Header
+struct Elf64_Shdr {
+    Elf64_Word    sh_name;
+    Elf64_Word    sh_type;
+    Elf64_Xword   sh_flags;
+    Elf64_Addr    sh_addr;
+    Elf64_Off     sh_offset;
+    Elf64_Xword   sh_size;
+    Elf64_Word    sh_link;
+    Elf64_Word    sh_info;
+    Elf64_Xword   sh_addralign;
+    Elf64_Xword   sh_entsize;
+};
+
+
+void write_elf_object_file(const std::string& output_filename, const std::vector<uint8_t>& code);
+
+#endif // ELF_H
